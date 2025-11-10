42a2ccaf8b6a69317523287a0f2d82a7c33df04c
Adrian-Lorca <alormor0107b@g.educaand.es>
2025-11-10 12:17:58 +0100

    Add files via upload

---
+++ Relacion1_Variables/actividad1.java
@@ -0,0 +1,20 @@
+package Relacion1_Variables;
+
+public class actividad1 {
+    public static void main(String[] args) {
+        int p, q;
+        char a, b, c;
+        long contador; 
+        int indice;
+        char car1, car2;
+        boolean logico = false;
+        boolean primero = false;
+        boolean ultimo = false;
+        float x, y, z;
+        double raiz1, raiz2;
+        short indicador;
+        double precio, precioFinal;
+        byte valor;
+        String nombre;
+    }
+}
---
+++ Relacion1_Variables/actividad3.java
@@ -0,0 +1,5 @@
+public class actividad3 {
+    public static void main(String[] args) {
+        
+    }
+}
